49cab6bfe4e70fd1407d291229d45c33b2035116
Beth Nakamura <bnak@umich.edu>
2016-12-18 21:21:36 -0800

    Main file and sample function

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+	print_string("This will print the string provided")
+	print "End of main function"
+	
+def print_string(sampleString):
+	print sampleString
+
+if __name__ == "__main__":
+	main()
